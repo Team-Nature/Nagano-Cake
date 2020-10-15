f386e99c68c5948031b41445b10baf412e579381
EC2 Default User <ec2-user@ip-172-31-60-68.ec2.internal>
2020-10-15 10:00:47 +0000

    controller

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,18 @@
+class Public::CustomersController < ApplicationController
+  
+  def show
+  end
+  
+  def quit
+  end
+  
+  def out
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+end
