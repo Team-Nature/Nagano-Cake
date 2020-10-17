e3e2022ceddfc16efa188e2a99451b309bdfd4a9
EC2 Default User <ec2-user@ip-172-31-43-133.ec2.internal>
2020-10-17 02:44:37 +0000

    first commit as fixed one

--- config/routes.rb
+++ config/routes.rb
@@ -58,8 +58,5 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :categories
   end
-
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
