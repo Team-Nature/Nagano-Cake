88c4fa1b2d6f0ee227ff4bdca58abc135fa605a9
EC2 Default User <ec2-user@ip-172-31-60-68.ec2.internal>
2020-10-18 02:58:39 +0000

    View

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   
     def after_sign_in_path_for(resource)
       if customer_signed_in?
-        items_path(resource)
+        items_path
       else
         admin_top_path
       end
