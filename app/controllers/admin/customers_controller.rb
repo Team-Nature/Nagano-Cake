76d03b08f5077b6d61af0d848b7accf03227fbc4
EC2 Default User <ec2-user@ip-172-31-60-68.ec2.internal>
2020-10-27 10:43:40 +0000

    css Pagin

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -2,26 +2,26 @@ class Admin::CustomersController < ApplicationController
   before_action :authenticate_admin!
 
   def update
-    @customer = Customer.find(params[:id])  
+    @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
       redirect_to admin_customer_path(@customer)
     else
       render "edit"
     end
   end
-  
+
   def show
     @customer = Customer.find(params[:id])
   end
-  
+
   def index
-    @customers = Customer.all
+    @customers = Customer.page(params[:page]).per(10)
   end
 
   def edit
     @customer = Customer.find(params[:id])
   end
-  
+
   private
     def customer_params
       params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postcode, :address, :tel, :email, :is_active)
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,19 +1,21 @@
 class Admin::OrdersController < ApplicationController
-  before_action :authenticate_admin! 
+  before_action :authenticate_admin!
   def index
-    @orders = Order.all.order(id: :desc)
-  end  
-  
+    # @orders = Order.all.order(id: :desc)
+    @orders = Order.page(params[:page]).per(10)
+
+  end
+
   def today
     @orders = Order.today
     render "index"
   end
-  
+
   def show
     @order = Order.find(params[:id])
     @customer = @order.customer
   end
-  
+
   def update
     order = Order.find(params[:id])
     if order.update(params_int(order_params))
@@ -25,7 +27,7 @@ class Admin::OrdersController < ApplicationController
     def order_params
       params.require(:order).permit(:status)
     end
-    
+
     def integer_string?(str)
       begin
         Integer(str)
@@ -34,7 +36,7 @@ class Admin::OrdersController < ApplicationController
         false
       end
     end
-    
+
     def params_int(model_params)
       model_params.each do |key, val|
         if integer_string?(val)
